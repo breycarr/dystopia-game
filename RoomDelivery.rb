ec97ba303fa78641b8feea6ea1245abb3eccbc67
Ben <43816668+reycarr@users.noreply.github.com>
2018-10-08 22:27:59 +0100

    Minor adjustments

--- RoomDelivery.rb
+++ RoomDelivery.rb
@@ -5,11 +5,11 @@ room = ["lamp","pen","phone charger","widescreen television"] #does there need t
 puts "You stand in the centre of a dimly lit room."
 puts "You have no real purpose in the room, but you can see a door AHEAD of you, as well as several items at which you could LOOK"
 puts "what would you like to do?"
-action = gets.downcase.chomp # weirdly I can't get this to accept user input in Atom, but I can in Repl
+action = gets.chomp # weirdly I can't get this to accept user input in Atom, but I can in Repl
 
-if action.empty? == true
+if action.downcase.empty? == true
   puts "well you're going to have to type something, or we'll never get anywhere"
-elsif action.include? "look"
+elsif action.downcase.include? "look"
   puts "You are standing in a dimly lit room, there are several items which look small enough to carry"
 else
   puts "I'm afraid I don't understand what you want, why not try again?"
