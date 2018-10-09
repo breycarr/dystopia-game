669e2d7080404c0a1bd032c79f0c3f5706ce036a
Ben <43816668+reycarr@users.noreply.github.com>
2018-10-09 21:49:10 +0100

    Simplified some text, added check function, added take function

--- RoomDelivery.rb
+++ RoomDelivery.rb
@@ -1,18 +1,59 @@
-inventory = ["broken pencil","pocket lint","trusty, but ultimately useless, penknife"]
-room = ["lamp","pen","phone charger","widescreen television"] #does there need to be a distinction for items in the room array that can be picked up, and can't be picked up?
+inventory = ["pencil","lint","penknife"]
+room = ["lamp","pen","charger"] #does there need to be a distinction for items in the room array that can be picked up, and can't be picked up?
 
-# this is going to be some introductory text, which invites user input
 puts "You stand in the centre of a dimly lit room."
-puts "You have no real purpose in the room, but you can see a door AHEAD of you, as well as several items at which you could LOOK"
+puts "You can see several object you might be able to pick up"
+puts " "
+puts "In your inventory is:"
+puts inventory
+puts " "
 puts "what would you like to do?"
-action = gets.chomp # weirdly I can't get this to accept user input in Atom, but I can in Repl
+action = gets.chomp #currently having to test run this code in Repl, as user input does not work in Atom
 
-if action.downcase.empty? == true
-  puts "well you're going to have to type something, or we'll never get anywhere"
-elsif action.downcase.include? "look"
-  puts "You are standing in a dimly lit room, there are several items which look small enough to carry"
-else
-  puts "I'm afraid I don't understand what you want, why not try again?"
-  action = gets.downcase.chomp
-  #from here loop back to start
+loop do
+  if action.downcase.empty? == true
+    puts "well you're going to have to type something, or we'll never get anywhere"
+    action = gets.chomp
+  elsif action.downcase.include? "check"
+    loop do
+      if action.downcase.include? "inventory"
+        puts "you are carrying"
+        puts inventory
+        break
+      elsif action.downcase.include? "room"
+        puts "in the room, you can see"
+        puts room
+        break
+      else
+        puts "you'll need to tell me what you want to check!"
+        action = gets.chomp
+      end
+    end
+    action = gets.chomp
+  elsif action.downcase.include? "take"
+    if action.split(" ").any? {|word| word == "lamp"} = true
+      room.delete!("lamp")
+      inventory.push!("lamp")
+      puts "You are now carrying"
+      puts inventory
+      break
+    else
+      puts "I can't do that"
+      break
+    end
+  elsif action.downcase.include? "drop"
+  elsif action.downcase.include? "ahead"
+    yield( ) #this would yield to room number two
+  else
+    puts "I'm afraid I don't understand what you want, why not try again?"
+    action = gets.chomp
+  end
 end
+
+=begin
+Within the above needs to be a function which allows the player to pick up or put down items
+1 Take is tied to the room array
+2 Drop is tied to the inventory array
+3 The item string should be compared to the relevant array
+4 If the string can be found in the array, it should be moved (or deleted) from one array and added to another
+=end
